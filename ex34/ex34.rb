effdfdceb6db78e3578c6b59215abf8de5f65793
Alix Hamilton <alix.hamilton@gmail.com>
2016-11-01 15:29:08 -0700

    completed ex34

---
+++ ex34/ex34.rb
@@ -0,0 +1,10 @@
+animals = ['bear', 'ruby', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+puts "The animal at 1 is #{animals[1]}: #{animals[1] == 'ruby'}"
+puts "The third animal is #{animals[2]}: #{animals[2] == 'peacock'}"
+puts "The first animal is #{animals[0]}: #{animals[0] == 'bear'}"
+puts "The animal at 3 is #{animals[3]}: #{animals[3] == 'kangaroo'}"
+puts "The fifth animal is #{animals[4]}: #{animals[4] == 'whale'}"
+puts "The animal at 2 is #{animals[2]}: #{animals[2] == 'peacock'}"
+puts "The sixth animal is #{animals[5]}: #{animals[5] == 'platypus'}"
+puts "The animal at 4 is #{animals[4]}: #{animals[4] == 'whale'}"
